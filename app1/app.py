48185fa3c550630751b2b94bf2d2a50368c60e84
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-08 10:35:09 +0100

    dev requests

--- app1/app.py
+++ app1/app.py
@@ -17,11 +17,27 @@ class tickets(db.Model):
 @app.route('/')
 @app.route('/home')
 def home():
+	return render_template('home.html', title='Home:')
+
+@app.route('/home/generate')
+def generate():
+	all_tickets=tickets.query.all()
 	letterstring = requests.get('http://app2:5001/letterstring')
 	primeno = requests.get('http://app3:5002/primeno')
 	checkstring = requests.post('http://app4:5003/checkstring', data=letterstring)
 	checkprime = requests.post('http://app4:5003/checkprime', data=primeno)
-	return render_template('home.html', title='Home:')
+	if (checkstring == 'True' & checkprime=='True'):
+		win=True
+		ticket = tickets(
+			letterstring=letterstring,
+			primeno=primeno,
+			win=win
+		)
+		db.session.add(ticket)
+		db.session.commit()
+	else:
+		win=False
+	return render_template('home.html', title='Home:',tickets=all_tickets)
 
 
 if __name__ == '__main__':
--- app4/app.py
+++ app4/app.py
@@ -6,13 +6,30 @@ app = Flask(__name__)
 
 @app.route('/checkstring', methods='POST')
 def checkstring():
+	result=False
 	checkstring = request.data.decode('utf-8')
+	if (checkstring.find('A') != -1): 
+		result=True
+	elif (checkstring.find('a') != -1): 
+		result=True
+	else: 
+		result=False
 	return Response(result, mimetype='text/plain')
 
 @app.route('/checkprime', methods='POST')
 def checkprime():
-	checkprime = request.data.decode('utf-8')
-	return Response(result, mimetype='text/plain')
+	num = request.data.decode('utf-8')
+	isprime = False
+	if num > 1:
+		for i in range(2,num):
+			if (num % i) == 0:
+				isprime = False
+				break
+			else:
+				isprime=True
+	else:
+		isprime = False
+	return Response(isprime, mimetype='text/plain')
 
 if __name__ == '__main__':
  app.run(debug=True, host='0.0.0.0')
