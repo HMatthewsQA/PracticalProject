a513af8ab856046914850379a8070e780298ff36
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-08 14:53:06 +0100

    changed app4 func

--- app4/app.py
+++ app4/app.py
@@ -6,29 +6,29 @@ app = Flask(__name__)
 
 @app.route('/checkstring', methods=['POST'])
 def checkstring():
-	result=False
+	result='False'
 	checkstring = request.data.decode('utf-8')
 	if (checkstring.find('A') != -1): 
-		result=True
+		result='True'
 	elif (checkstring.find('a') != -1): 
-		result=True
+		result='True'
 	else: 
-		result=False
+		result='False'
 	return Response(result, mimetype='text/plain')
 
 @app.route('/checkprime', methods=['POST'])
 def checkprime():
 	num = int(request.data.decode('utf-8'))
-	isprime = False
+	isprime = 'False'
 	if num > 1:
 		for i in range(2,num):
 			if (num % i) == 0:
-				isprime = False
+				isprime = 'False'
 				break
 			else:
-				isprime=True
+				isprime='True'
 	else:
-		isprime = False
+		isprime = 'False'
 	return Response(isprime, mimetype='text/plain')
 
 if __name__ == '__main__':
