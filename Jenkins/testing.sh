f8f4c4e45fd95bb625347a58f1188f1d92c5998e
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-10 12:14:03 +0100

    venv installs

--- Jenkins/testing.sh
+++ Jenkins/testing.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+sudo apt update -y
+sudo apt install python3-pip python3-venv -y
+
 cd PracticalProject
 python3 -m venv vevnv
 . ./venv/bin/activate
-
-pytest ./app1
-pytest ./app2
-pytest ./app3
-pytest ./app4
+pip3 install flask flask-testing pytest pytest-cov requests Werkzeug==0.16.1 flask_sqlalchemy
+pytest ./app1/tests/test_unit.py
+pytest ./app2/tests/test_unit.py
+pytest ./app3/tests/test_unit.py
+pytest ./app4/tests/test_unit.py
 
 deactivate
