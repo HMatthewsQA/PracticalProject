912dda21ac11c42f608da8360132b7122cf7e56f
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-10 12:18:12 +0100

    venv source

--- Jenkins/testing.sh
+++ Jenkins/testing.sh
@@ -4,7 +4,7 @@ sudo apt install python3-pip python3-venv -y
 
 cd PracticalProject
 python3 -m venv vevnv
-. ./venv/bin/activate
+. venv/bin/activate
 pip3 install flask flask-testing pytest pytest-cov requests Werkzeug==0.16.1 flask_sqlalchemy
 pytest ./app1/tests/test_unit.py
 pytest ./app2/tests/test_unit.py
