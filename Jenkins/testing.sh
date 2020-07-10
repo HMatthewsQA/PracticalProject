a89aeacb606a49ceded2d9ee6e4cbe3a3fd9b367
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-10 12:25:00 +0100

    fixed typo

--- Jenkins/testing.sh
+++ Jenkins/testing.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 sudo apt install python3-pip python3-venv -y
 
-python3 -m venv vevnv
+python3 -m venv venv
 . venv/bin/activate
 pip3 install flask flask-testing pytest pytest-cov requests Werkzeug==0.16.1 flask_sqlalchemy
 pytest ./app1/tests/test_unit.py
