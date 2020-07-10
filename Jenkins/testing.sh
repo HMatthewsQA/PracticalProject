9f720ec23a930fd1701ca12b87ad36352cc457db
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-10 12:08:16 +0100

    cd venv

--- Jenkins/testing.sh
+++ Jenkins/testing.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cd PracticalProject
 python3 -m venv vevnv
 . ./venv/bin/activate
 
-pytest ./PracticalProject/app1
-pytest ./PracticalProject/app2
-pytest ./PracticalProject/app3
-pytest ./PracticalProject/app4
+pytest ./app1
+pytest ./app2
+pytest ./app3
+pytest ./app4
 
 deactivate
